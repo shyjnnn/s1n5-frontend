e02a06e369a7a273d64a25ccb645102ba8827d22
shyjnnn <shinyejin0212@gmail.com>
2024-05-18 02:35:23 +0000

    feat: fix onboarding

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
+import Image from 'next/image';
 import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
@@ -10,12 +11,26 @@ import { theme } from '@/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+  const [click, setClick] = useState<boolean>(false);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <MainLayout>
-          <Component {...pageProps} />
+          {!click ? (
+            <Image
+              src="/png/onboard.png"
+              alt="onboard"
+              width={375}
+              height={852}
+              onClick={() => {
+                setClick(true);
+              }}
+            />
+          ) : (
+            <Component {...pageProps} />
+          )}
         </MainLayout>
         <ReactQueryDevtools initialIsOpen={false} />
       </ThemeProvider>
